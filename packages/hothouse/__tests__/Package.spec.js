f0e9a7a479d1f99fd548210d2ad29cb5f75aaab7
Leko <leko.noor@gmail.com>
2018-07-01 00:10:59 +0900

    :white_check_mark: Add prerelease tests

--- packages/hothouse/__tests__/Package.spec.js
+++ packages/hothouse/__tests__/Package.spec.js
@@ -20,3 +20,12 @@ test("replaceSemver can replace tilde semver range", () => {
 test("replaceSemver can replace hat semver range", () => {
   assert.equal(replaceSemver("^1.13.1", "2.0.0"), "^2.0.0");
 });
+test("replaceSemver can replace semver range with prerelease (like Babel)", () => {
+  assert.equal(
+    replaceSemver("^7.0.0-beta.49", "7.0.0-beta.51"),
+    "^7.0.0-beta.51"
+  );
+});
+test("replaceSemver throws Error when too many diffs", () => {
+  assert.throws(() => replaceSemver("^1.0.0-beta.1", "1.0.0-beta.202"));
+});
