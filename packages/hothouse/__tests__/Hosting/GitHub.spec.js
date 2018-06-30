81f8073f9e3b23bdb66cada8642b0b9f91e7ca47
Leko <leko.noor@gmail.com>
2018-07-01 01:30:57 +0900

    :white_check_mark: Add parseRepositoryUrl tests

--- packages/hothouse/__tests__/Hosting/GitHub.spec.js
+++ packages/hothouse/__tests__/Hosting/GitHub.spec.js
@@ -1,9 +1,21 @@
 /* eslint-env jest */
 // @flow
 import assert from "assert";
-import parseGithubRepoUrl from "parse-github-repo-url";
+import { parseRepositoryUrl } from "../../src/Hosting/GitHub";
 
-test("`parse-github-repo-url` can parse GitHub url", () => {
-  const [owner, repo] = parseGithubRepoUrl("https://github.com/Leko/hothouse");
+test("parseRepositoryUrl can git+ssh url", () => {
+  const [owner, repo] = parseRepositoryUrl(
+    "git+ssh://git@github.com/Leko/hothouse.git"
+  );
   assert.deepStrictEqual([owner, repo], ["Leko", "hothouse"]);
+});
+test("parseRepositoryUrl can basic http url", () => {
+  const [owner, repo] = parseRepositoryUrl("https://github.com/Leko/hothouse");
+  assert.deepStrictEqual([owner, repo], ["Leko", "hothouse"]);
+});
+test("parseRepositoryUrl can parse monorepo url", () => {
+  const [owner, repo] = parseRepositoryUrl(
+    "https://github.com/babel/babel/tree/master/packages/babel-cli"
+  );
+  assert.deepStrictEqual([owner, repo], ["babel", "babel"]);
 });
